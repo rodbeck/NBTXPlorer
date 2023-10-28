af9420be7a44783480ab0fe66e9e68cd903e2b1d
Rodolphe Beck <rodolphe.beck@outlook.com>
2023-10-28 13:31:28 +0200

    Initial Commit

---
+++ NBTXplorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NBTXplorer
+//
+//  Created by Rodolphe Beck on 28/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NBTXplorer/NBTXplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBTXplorerApp.swift
+//  NBTXplorer
+//
+//  Created by Rodolphe Beck on 28/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct NBTXplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
